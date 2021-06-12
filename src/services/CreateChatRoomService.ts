08aa19aa0457be5371d908671123010099a673ff
lucas <lucasfonsecard@icloud.com>
2021-06-12 06:17:13 -0300

    fix: update room creation service
    Now it only creates a new room if the users don't already share one

--- src/services/CreateChatRoomService.ts
+++ src/services/CreateChatRoomService.ts
@@ -1,10 +1,22 @@
+import { ObjectId } from 'mongoose';
 import { injectable } from 'tsyringe';
 
 import { ChatRoom } from '../schemas/ChatRoom';
 
 @injectable()
 export class CreateChatRoomService {
-  public async execute(users: string[]): Promise<ChatRoom> {
-    return ChatRoom.create({ users });
+  public async execute(idUsers: ObjectId[]): Promise<ChatRoom> {
+    const findUsersRoom = await ChatRoom.findOne({
+      users: {
+        $all: idUsers,
+      },
+    }).exec();
+
+    if (findUsersRoom) {
+      console.log('has room');
+      return findUsersRoom;
+    }
+
+    return ChatRoom.create({ users: idUsers });
   }
 }
